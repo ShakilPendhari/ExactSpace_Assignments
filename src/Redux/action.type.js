78d4fd4fa464666a87234c0e24e25246e509c95c
Shakil <shakilpp123456@gmail.com>
2023-09-04 12:18:32 +0530

    checking commit in chat app

--- src/Redux/action.type.js
+++ src/Redux/action.type.js
@@ -1,3 +1,3 @@
-// export const LOADING = "LOAIDING"
-// export const ERROR = "ERROR"
-// export const ADD_DATA = "ADD_DATA"
+export const LOADING = "LOAIDING"
+export const ERROR = "ERROR"
+export const ADD_DATA = "ADD_DATA"
